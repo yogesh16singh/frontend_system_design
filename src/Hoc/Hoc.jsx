8cf51376585f7d6dd4504317add1631491ff1928
yogesh16singh <ys1997642@gmail.com>
2024-01-12 11:54:49 +0530

    initial setup

---
+++ src/Hoc/Hoc.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hoc = () => {
+    return (
+        <div className='bg-gray-500'>
+            <h1>Higher Order Components</h1>
+
+        </div>
+    )
+}
+
+export default Hoc
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Hoc from './Hoc/Hoc';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Hoc></Hoc>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
